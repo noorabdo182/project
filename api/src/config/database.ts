d9dec4488643096c66a5e13c4515a560421d0ca6
nnorabdo182 <na6342315@gmail.com>
2026-08-15 17:28:45 +0400

    first commit

---
+++ api/src/config/database.ts
@@ -0,0 +1,11 @@
+
+import mongoose from "mongoose";
+
+export async function connectToDatabase(
+mongoUri: string
+): Promise<void> {
+
+await mongoose.connect (mongoUri);
+
+console. log("MongoDB connected");
+}
